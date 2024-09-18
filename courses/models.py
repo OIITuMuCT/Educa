4d61a9fb2c0c92b248204e415b28b87e9d55a9e4
OIITuMuCT <vataman@live.ru>
2024-09-18 19:37:27 +0300

    adding ordering to Module and Content objects

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from datetime import timezone
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -46,3 +48,50 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name='contents',
+        on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={ 'model__in': (
+                        'text',
+                        'image',
+                        'video',
+                        'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        abstract = True
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Text(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='image')
+
+class Video(ItemBase):
+    url = models.URLField()
