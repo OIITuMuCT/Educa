970215edc64da6f71882b59ccb7bd91dbce9bd0f
OIITuMuCT <vataman@live.ru>
2024-09-21 22:43:09 +0300

    register student form is work

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.StudentRegistrationView.as_view(), name='students_registration'),
-    
+    path(
+        "register/",
+        views.StudentRegistrationView.as_view(),
+        name="student_registration",
+    ),
 ]
