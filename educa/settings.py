10f3307effa37f278dfef0a24c814e4951501442
OIITuMuCT <vataman@live.ru>
2024-09-26 14:41:49 +0300

    add datetime in chat

--- educa/settings.py
+++ educa/settings.py
@@ -174,3 +174,13 @@ REST_FRAMEWORK = {
 
 # ASGI_APPLICATION = "educa.routing.application"
 ASGI_APPLICATION = "educa.asgi.application"
+
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
