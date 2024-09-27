a79dbd9fe8b3dc0965da17b5ff1650528d761798
OIITuMuCT <vataman@live.ru>
2024-09-27 18:01:21 +0300

    docker up not worked

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 DEBUG = False
@@ -6,6 +7,30 @@ ADMINS = [
 ]
 ALLOWED_HOSTS = ["*"]
 
+# DATABASES = {
+#     "default": {}
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('POSTGRES_DB'),
+#         'USER': config('POSTGRES_USER'),
+#         'PASSWORD': config('POSTGRES_PASSWORD'),
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
-    "default": {}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
 }
+
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
